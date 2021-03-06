2ff27ae9000ec397688c3a67d666d65d408b931b
yurareshetov <darkaskyl@mail.ru>
2021-03-06 11:22:23 +0300

    ЛАБА

--- kropotova_lab/Program.cs
+++ kropotova_lab/Program.cs
@@ -6,55 +6,90 @@ using System.Threading.Tasks;
 
 namespace kropotova_lab
 {
-    class Program
+    class Program 
+    { 
+
+
+         static void Main(string[] args)
+         {
+
+            //ExceptionTest(); 
+            //ExceptionTest2(); 
+            //ExceptionTest3()
+            //ExceptionTest4()
+
+            Console.ReadKey();
+         }
+         static void ExceptionTest()
     {
-        static void Main(string[] args)
-        {
-            {
-               // Console.WriteLine(DateTime.Now);
-                //      var MassivDate = new List<DateTime>() {
-                //     new DateTime(2021, 1, 15),
-                //     new DateTime(2021, 2, 20),
-                //      new DateTime(2011, 5, 2),
-                //    new DateTime(2015, 5, 1),
-                //  new DateTime(2013, 5, 3),
-                //       new DateTime(2020, 1, 17)};
-                //      var result = new Dictionary<int, int>();
-                //     foreach (DateTime NastData in MassivDate)
-                //    {
-                //      if (result.ContainsKey(NastData.Month))
-                //    {
-                //      result[NastData.Month] = result[NastData.Month] + 1;
-                //           }
-                //         else
-                //          result[NastData.Month] = 1;
-                //    }
-                //             var PopularMonth = -1;
-                //           var MaxCount = 0;
-                //         foreach (KeyValuePair<int, int> keyValue in result)
-                //       {
-                //         if (keyValue.Value > MaxCount)
-                //       {
-                //         MaxCount = keyValue.Value;
-                //       PopularMonth = keyValue.Key;
-                // }
-                // }
-
-
-                //      Console.WriteLine($"Самый популярный месяц {PopularMonth}");
-                //       Console.Write("Press ENTER to continue...");
-                //      Console.ReadLine();
-
-
-         //       DateTime date1 = new DateTime(2021, 2, 20 ); 
-         //       DateTime date2 = new DateTime(2021, 2, 23 ); 
-         //       Console.WriteLine(Math.Abs(date1.Subtract(date2).TotalDays));
-         //       Console.ReadLine();
-
-
-
-
-             }
-          }
-       }
+        int x = 5;
+        int y = x / 0;
+        Console.WriteLine($"Результат: {y}");
+        Console.WriteLine("Конец программы");
+        Console.Read();
     }
+         static void ExceptionTest2()
+         {
+            try
+            {
+                Person p = new Person { Name = "Tom", Age = 17 };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            Console.Read();
+        }
+         static void ExceptionTest3() 
+        {
+            try
+            {
+                TestClass.Method1();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Catch в Main : {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally в Main");
+            }
+            Console.WriteLine("Конец метода Main");
+            Console.Read();
+        }
+         static void ExceptionTest4()
+        {
+              class TestClass
+        
+            public static void Method1()
+            {
+                try
+                {
+                    Method2();
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine($"Catch в Method1 : {ex.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine("Блок finally в Method1");
+                }
+                Console.WriteLine("Конец метода Method1");
+            }
+        static void Method2()
+        {
+            try
+            {
+                int x = 8;
+                int y = x / 0;
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally в Method2");
+            }
+            Console.WriteLine("Конец метода Method2");
+            }
+        }
+    }  
+}
