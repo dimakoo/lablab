39ed9b51bf54dd6cbc38682cedef547735aedbac
Nikita Malinin <malinin12513@gmail.com>
2021-03-13 10:57:22 +0300

    42

--- oap_labs/Program.cs
+++ oap_labs/Program.cs
@@ -10,41 +10,7 @@ namespace oap_labs
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            var MassivDate = new
-                List<DateTime>()
-        {
-            new DateTime (2021, 1, 15),
-            new DateTime (2021, 2, 20),
-            new DateTime (2011, 8, 2),
-            new DateTime (2015, 6, 1),
-            new DateTime (2013, 4, 3),
-            new DateTime (2020, 8, 17) };
-            var result = new Dictionary<int, int>();
-            foreach (DateTime TekushayaData in MassivDate)
-            {
-                if (result.ContainsKey(TekushayaData.Month))
-                {
-                    result[TekushayaData.Month] = result[TekushayaData.Month] + 1;
-                }
-                else result[TekushayaData.Month] = 1;
-            }
-            var PopularMonth = -1;
-            var MaxCount = 0;
-            foreach (KeyValuePair<int, int> keyValue in result)
-            {
-                if (keyValue.Value > MaxCount)
-                {
-                    MaxCount = keyValue.Value;
-                    PopularMonth = keyValue.Key;
-                }
-            }
-        
-        Console.WriteLine($"популярный месяц {PopularMonth}");
-            Console.Write("Press ENTER to countinue...");
-            Console.ReadLine();
-        }
+        static void Main(string[] args){
+	}
     }
 }
-//первая задача выполнена
